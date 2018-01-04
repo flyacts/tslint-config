d8b780fb8dd4a54f498181b5dc442e6763a1fcbc
Tim Schumacher <tim.schumacher@flyacts.com>
2018-01-04 15:35:36 +0100

    update copyright

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ module.exports = {
         "arrow-return-shorthand": [true],
         "class-name": true,
         "comment-format": [true, "check-space"],
-        "file-header": [true, "@copyright FLYACTS GmbH 2017"],
+        "file-header": [true, "@copyright FLYACTS GmbH 2018"],
         "no-consecutive-blank-lines": [true, 2],
         "no-unnecessary-callback-wrapper": true,
         "one-variable-per-declaration": [true, "ignore-for-loop"],
